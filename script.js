cf34b1b0aa1cf5838b7d3edaac9b5b79c05ae0a8
chloeachan <67658636+chloeachan@users.noreply.github.com>
2022-07-29 00:08:56 -0400

    added files

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// window.onload = function(){
+// document.querySelector('.cont_modal').className = "cont_modal";    
+// }
+// var c = 0;
+// function open_close(){
+//   if(c % 2 == 0){    
+// document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";  
+// c++;
+//   }else {
+// document.querySelector('.cont_modal').className = "cont_modal";  
+// c++;    
+//   }  
+// } 
+
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// let count = 0;
+// toggleButton.addEventListener('click', () => {
+//   navbarLinks.classList.toggle('active');
+//   count += 1;
+//   console.log(count);
+// });
+
+
+
